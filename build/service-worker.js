3ee5b7aab84e0eae70c45e81fcce095dd549cf59
Kartik Verma <kvs1297@gmail.com>
2019-10-09 18:49:36 +0530

    made some changes

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.e8be44adbebdfed152ceed7a2d18b4ac.js"
+  "/precache-manifest.83a4d1cdb22aae7761b843dd160b9fcf.js"
 );
 
 self.addEventListener('message', (event) => {
--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React, {userState} from 'react';
 import logo from './logo.svg';
 import './App.css'
 import {useRoutes} from 'hookrouter'
-import {useLoginContext} from 'context'
 import { routes } from 'routes';
 import { NotFoundPage } from 'components/NotFound/NotFoundPage';
 import { Footer } from 'components/Footer/Footer';
